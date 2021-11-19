3781258fc5bb24343a45f28f0a71144ac8735ac5
nemviktor <wookie7373@gmail.com>
2021-11-19 22:19:30 +0100

    Adding basic header.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+
+          <Route exact path="/">
+            <React.Fragment>
+              hello
+            </React.Fragment>
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+
+ const Header = () => {
+    return (
+        <header>
+            <h1>
+                <img src="https://fontmeme.com/permalink/210830/3ba538cc07d53b8aca892fa862216891.png" alt="pokemon-font" border="0"></img>
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
