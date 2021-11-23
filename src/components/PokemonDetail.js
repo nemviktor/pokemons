0375e4692670e9258ac11f099162734d74a69fc7
nemviktor <wookie7373@gmail.com>
2021-11-23 12:29:05 +0100

    Adding a pokeball to detailed view. Catching pokemons with the ball will be implemented the ball.

--- src/components/PokemonDetail.js
+++ src/components/PokemonDetail.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import '../scss/PokemonDetailed.scss';
+import Pokeball from "../img/Pokeball.png";
 
 
 const PokemonDetail = _ => {
@@ -20,6 +21,9 @@ const PokemonDetail = _ => {
 
     return (
         <div className="detailed-pokemon-container">
+            <button className="catch-btn" type="button">
+                <img src={ Pokeball } ></img>
+            </button>
             <img src={pokemonData.imageUrl} alt={pokemonData.name} />
             <div className="pokemon-detailed-properties">
                 <h2>{pokemonData.name}</h2>
